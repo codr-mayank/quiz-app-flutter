194f005f00f06f59dcc0c5a79e7cefe8418c4e8a
codr_mayank <gamblergamer9@gmail.com>
2020-05-20 23:35:29 +0530

    add question bank list

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/model/question.dart';
 
 class QuizApp extends StatefulWidget {
   @override
@@ -6,19 +7,50 @@ class QuizApp extends StatefulWidget {
 }
 
 class _QuizAppState extends State<QuizApp> {
+  List questionBank = [
+    Question.name('Earth has only 1 moon as a natural satellite', true),
+    Question.name(
+        'Cyclones spin in a clockwise direction in the southern hemisphere',
+        true),
+    Question.name('Goldfish only have a memory of three seconds', false),
+    Question.name('The capital of Libya is Benghazi', false),
+    Question.name(
+        'Brazil is the only country in the Americas to have the official language of Portuguese',
+        true),
+    Question.name(
+        'The Channel Tunnel is the longest rail tunnel in the world', false),
+    Question.name(
+        'Darth Vader famously says the line “Luke, I am your father” in The Empire Strikes Back',
+        false),
+    Question.name('Stephen Hawking declined a knighthood from the Queen', true),
+    Question.name('The highest mountain in England is Ben Nevis', false),
+    Question.name(
+        'Nicolas Cage and Michael Jackson both married the same woman', true),
+    Question.name(
+        'Japan and Russia did not sign a peace treaty after World War Two so are technically still at war',
+        true),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('True Citizen Quiz'),
+        title: Text('GK Quiz'),
         centerTitle: true,
         backgroundColor: Colors.blueGrey,
       ),
       backgroundColor: Colors.lightGreenAccent,
       body: Container(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image.asset('images/flag.png', width: 250, height: 180)
+            Center(
+                child: Image.asset('images/flag.png', width: 250, height: 180)),
+            Container(
+              height: 120.0,
+              child: Text(questionBank[5].questionText),
+            ),
+            Spacer(),
           ],
         ),
       ),
